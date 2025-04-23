fa6afb71c9bd25073711a493968a4eeb582cdb79
P . A Ishara Deshan Appuhami <110235551+IsharaDeshan6@users.noreply.github.com>
2025-04-23 11:34:55 +0530

    Initial commit

---
+++ src/main/java/com/em/nsbmemploymanagement/NsbmEmployManagementApplication.java
@@ -0,0 +1,13 @@
+package com.em.nsbmemploymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NsbmEmployManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NsbmEmployManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/em/nsbmemploymanagement/NsbmEmployManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.em.nsbmemploymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NsbmEmployManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
